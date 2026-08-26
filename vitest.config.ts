630f5c2448f6b726207a037a55a7f23e97dd604c
Steffen Aas <steffen.aas@outlook.com>
2026-08-26 03:14:53 +0200

    fiks husky block if no tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         environment: "jsdom",
         setupFiles: ["./vitest.setup.ts"],
         globals: true,
+        passWithNoTests: true,
     },
     resolve: {
         alias: {
